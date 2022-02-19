5846a7a62160707831e5bb22c2c384e4463907c0
senih <senihahmetuzun@gmail.com>
2022-02-18 22:07:02 -0500

    test 2/18

--- PatientSheetSoftware/src/main/App.java
+++ PatientSheetSoftware/src/main/App.java
@@ -1,5 +1,7 @@
 package main;
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.io.IOException;
 
 import javax.swing.SwingUtilities;
@@ -7,7 +9,9 @@ import javax.swing.SwingUtilities;
 public class App {
 
 	public static void main(String[] args) {
-		
+
+		System.out.println("Test");
+
 		SwingUtilities.invokeLater(new Runnable() {
 
 			@Override
